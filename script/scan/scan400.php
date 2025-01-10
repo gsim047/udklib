a8171262aa4ce2622156f8bc29e5221c0e9471e5
G Stepanov <gsim047@gmail.com>
2025-01-11 02:24:48 +0300

    fix g770/win zhd

--- script/scan/scan400.php
+++ script/scan/scan400.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once("cfg.php");
+
+/*
 function h1()
 {
 	echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n";
@@ -17,19 +20,21 @@ function h2()
 	echo "  </body>\n";
 	echo "</html>\n";
 }
-
+*/
 	
 // start
 
 	$dt = date("Ymd-His");
+	$colorTitle = colorText();
 	//echo "scan-$dt.jpg\n\n";
-	$addr = "snapscan:libusb:001:006";
+	$addr = scannerAddr(); //"snapscan:libusb:001:006";
 
-	h1();
-	echo "<h3>Scanner start... <br/>Image: 'scan-$dt.jpg'</h3><br/><br/>\n";
-	echo "Внимание! После долгого простоя, сканер прогревается до 45 секунд! Подождите немного...<br/>\n";
+	h1("Сканирование 400 dpi");
 
-	echo "<br/><a href=\"/ex.php?gal=scan\"><b>Перейти к сканам &gt;&gt;</b></a><br/>";
+	echo "<h3><font color=#$colorTitle >Scanner start... <br/>Image: 'scan-$dt.jpg'<br/><br/>\n";
+	echo "Внимание! После долгого простоя, сканер прогревается до 45 секунд! Подождите немного...<br/></font></h3>\n";
+
+	//echo "<br/><a href=\"/ex.php?gal=scan\"><b>Перейти к сканам &gt;&gt;</b></a><br/>";
 //	system("whoami");
 	system("/usr/bin/scanimage -d $addr --format=jpeg --resolution 400 -o /var/www/html/scan_/scan/scan-$dt.jpg >/dev/null 2>&1 &");
 
